e13c0d0a816c08b91335164b444e5466e5deca23
Gabriella Alves <gabiraiolase3785@gmail.com>
2026-06-23 17:45:48 -0300

    algumas atualizações da landing page

--- js/script.js
+++ js/script.js
@@ -61,3 +61,23 @@ document.querySelectorAll(
     observer.observe(element);
 
 });
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+
+    reveals.forEach(item => {
+
+        const windowHeight = window.innerHeight;
+        const elementTop = item.getBoundingClientRect().top;
+
+        if(elementTop < windowHeight - 100){
+            item.classList.add("active");
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
