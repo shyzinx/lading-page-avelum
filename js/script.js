70472f4e0f3a1c686fbfa60f0d2b6cede10e7db6
Gabriella Alves <gabiraiolase3785@gmail.com>
2026-06-23 17:54:56 -0300

    voltando o js antigo

--- js/script.js
+++ js/script.js
@@ -62,22 +62,3 @@ document.querySelectorAll(
 
 });
 const reveals = document.querySelectorAll(".reveal");
-
-function revealOnScroll(){
-
-    reveals.forEach(item => {
-
-        const windowHeight = window.innerHeight;
-        const elementTop = item.getBoundingClientRect().top;
-
-        if(elementTop < windowHeight - 100){
-            item.classList.add("active");
-        }
-
-    });
-
-}
-
-window.addEventListener("scroll", revealOnScroll);
-
-revealOnScroll();
